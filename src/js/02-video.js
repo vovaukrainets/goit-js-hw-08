8924b4d6479ee2bea9b2bb8654319ebf33366387
vovaukrainets <vovaukrainets@gmail.com>
2021-12-27 00:43:31 +0200

    Homework

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = `videoplayer-current-time`;
+const currentTime = localStorage.getItem(STORAGE_KEY);
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+   
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(data) {
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+};
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+    }).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+
+        default:
+            break;
+    }
+});
