d9a8284397344514d810c81481e4d302c7463cf7
vovaukrainets <vovaukrainets@gmail.com>
2021-12-27 00:47:58 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = `feedback-form-state`;
+
+const form = document.querySelector(`.feedback-form`);
+const email = document.querySelector(`.feedback-form input`);
+const message = document.querySelector(`.feedback-form textarea`);
+
+form.addEventListener(`input`, throttle(onTextareaInput, 500));
+form.addEventListener(`submit`, onFormSubmit);
+
+populateTextarea();
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+
+    form.reset();
+    localStorage.removeItem(STORAGE_KEY);    
+ }
+
+function onTextareaInput(evt) {
+    const formData = {...JSON.parse(localStorage.getItem(STORAGE_KEY))};
+    formData[evt.target.name] = evt.target.value;
+    const formDataJSON = JSON.stringify(formData);
+      
+    localStorage.setItem(STORAGE_KEY, formDataJSON);
+}
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);    
+
+    if (savedMessage) {
+        const parseSavedMessage = JSON.parse(savedMessage);
+        email.value = parseSavedMessage.email || "";
+        message.value = parseSavedMessage.message || "";
+    }
+}
